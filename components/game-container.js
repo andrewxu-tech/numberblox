1f2d4c574ec545121d997853645a20c7360e1a18
Andrew Xu <alexandrew94@hotmail.com>
2018-07-02 15:47:57 +0100

    made a grid

--- components/game-container.js
+++ components/game-container.js
@@ -1,12 +1,17 @@
 import React from 'react';
-import { StyleSheet, View, Dimensions } from 'react-native';
+import { StyleSheet, View, Dimensions, Alert } from 'react-native';
 
 export default class App extends React.Component {
   render() {
     return (
       <View>
         <View style={styles.container}>
-          <View style={styles.box}/>
+          <View
+            style={styles.box}
+            onPress={() => {
+              Alert.alert('You tapped the button!');
+            }}
+          />
           <View style={styles.box}/>
           <View style={styles.box}/>
           <View style={styles.box}/>
@@ -25,11 +30,16 @@ const styles = StyleSheet.create({
   container: {
     height: 500,
     width: Dimensions.get('window').width,
-    backgroundColor: 'green'
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    flexWrap: 'wrap',
+    paddingTop: 10
   },
   box: {
-    height: 50,
-    width: 50,
-    backgroundColor: 'red'
+    height: (Dimensions.get('window').width / 3) - 10,
+    width: (Dimensions.get('window').width / 3) - 10,
+    backgroundColor: 'red',
+    marginBottom: 10,
+    borderRadius: 15
   }
 });
