8f42c428797b0c463991b110c6a026dbddb08559
Andrew Xu <alexandrew94@hotmail.com>
2018-07-14 18:29:53 +0100

    Added a font

--- components/game-container.js
+++ components/game-container.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { StyleSheet, View, Dimensions, TouchableOpacity, Text } from 'react-native';
+import { Font } from 'expo';
 
 export default class App extends React.Component {
 
   state = {
     currentSquares: [],
     pressed: true,
-    gameOver: false
+    gameOver: false,
+    loaded: false
   }
 
   generateRandomSquares = (numberOfSquares) => {
@@ -27,6 +29,13 @@ export default class App extends React.Component {
     }, 1500);
   }
 
+  async componentWillMount() {
+    await Font.loadAsync({
+      DosisExtraBold: require('../assets/fonts/Dosis-ExtraBold.ttf')
+    });
+    this.setState({ ...this.state, loaded: true });
+  }
+
   componentDidMount = () => {
     this.generateRandomSquares(3);
     this.timer();
@@ -35,28 +44,36 @@ export default class App extends React.Component {
   render() {
     return (
       <View>
-        <Text>{this.state.gameOver}</Text>
-        <Text>{this.state.currentSquares}</Text>
-        <View style={styles.container}>
-          {[...Array(9)].map((e, i) => {
-            return <View key={i}>
-              {this.state.currentSquares.includes(i) &&
-                <TouchableOpacity
-                  style={styles.specialBox}
-                  onPress={() => {
-                    this.props.handlePress();
-                    this.setState({ ...this.state, pressed: true });
-                  }}
-                />
-              }
-              {!this.state.currentSquares.includes(i) &&
-                <View
-                  style={styles.box}
-                />
-              }
-            </View>;
-          })}
-        </View>
+        {this.state.loaded &&
+          <View>
+            <Text>{this.state.gameOver}</Text>
+            <Text>{this.state.currentSquares}</Text>
+            <View style={styles.container}>
+              {[...Array(9)].map((e, i) => {
+                return <View key={i}>
+                  {this.state.currentSquares.includes(i) &&
+                    <TouchableOpacity
+                      style={styles.specialBox}
+                      onPress={() => {
+                        this.props.handlePress();
+                        this.setState({ ...this.state, pressed: true });
+                      }}
+                    >
+                      <Text style={styles.specialBoxText}>
+                        {this.state.currentSquares.indexOf(i) + 1}
+                      </Text>
+                    </TouchableOpacity>
+                  }
+                  {!this.state.currentSquares.includes(i) &&
+                    <View
+                      style={styles.box}
+                    />
+                  }
+                </View>;
+              })}
+            </View>
+          </View>
+        }
       </View>
     );
   }
@@ -83,6 +100,14 @@ const styles = StyleSheet.create({
     width: (Dimensions.get('window').width / 3) - 10,
     backgroundColor: 'green',
     marginBottom: 10,
-    borderRadius: 15
+    borderRadius: 15,
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    flexDirection: 'column'
+  },
+  specialBoxText: {
+    fontSize: 72,
+    fontFamily: 'DosisExtraBold'
   }
 });
